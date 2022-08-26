6493e7a96265d7412da8276081e3f5d25f93a4c5
Alex <blyxyas@gmail.com>
2022-08-26 23:05:57 +0200

    Added README + .gitignore

--- out.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-	println!("Hello world!");
-	println!("Hello world!");
-	println!("HHQ9");
-	let mut current = 99; while current >= 1 { println!("{} bottles of beer\nyou take one down, pass it around,\n{} bottles of beer on the wall.\n", current, current - 1); current-= 1; }
-	}
